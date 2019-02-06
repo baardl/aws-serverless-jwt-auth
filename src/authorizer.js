37b3812b8a37b9840b0f87d2fa84b622f78cadd8
baardl <bard.lind@gmail.com>
2019-02-06 09:36:56 +0100

    Remove original example handler

--- src/authorizer.js
+++ src/authorizer.js
@@ -94,19 +94,6 @@ function decodeFromIdToken(accessToken) {
       */
 }
 
-exports.handler = async function (event) {
-  const token = event.authorizationToken.toLowerCase()
-  const methodArn = event.methodArn
-
-  switch (token) {
-    case 'allow':
-      return generateAuthResponse('user', 'Allow', methodArn)
-    case 'deny':
-      return generateAuthResponse('user', 'Deny', methodArn)
-    default:
-      return Promise.reject('Error: Invalid token') // Returns 500 Internal Server Error
-  }
-}
 
 function generateAuthResponse (principalId, effect, methodArn) {
   // If you need to provide additional information to your integration
